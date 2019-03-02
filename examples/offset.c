e12fa57471cc068a7f11ec0d507e053ae9c11900
johnnylord <johnnylord7410@gmail.com>
2019-03-02 21:53:47 +0800

    Fix missing semicolon and wrong type arguments

--- examples/offset.c
+++ examples/offset.c
@@ -4,16 +4,16 @@
 
 struct list_head {
     struct list_head *next, *prev;
-}
+};
 
 struct A {
     char c[24];
     struct list_head list;
-}
+};
 
 int main()
 {
     printf("Offset from list elment's address to struct A's address: %d\n",
-           (int) offsetof(A, list));
+           (int) offsetof(struct A, list));
     return 0;
 }
