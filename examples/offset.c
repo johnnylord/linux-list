2f74171155e8ffb41423b72d59e4d9eb3ec813fc
johnnylord <johnnylord7410@gmail.com>
2019-03-02 21:51:32 +0800

    Add an example showing how offsetof macro works

---
+++ examples/offset.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#define offsetof(TYPE, MEMBER) ((size_t) & ((TYPE *) 0)->MEMBER)
+
+struct list_head {
+    struct list_head *next, *prev;
+}
+
+struct A {
+    char c[24];
+    struct list_head list;
+}
+
+int main()
+{
+    printf("Offset from list elment's address to struct A's address: %d\n",
+           (int) offsetof(A, list));
+    return 0;
+}
